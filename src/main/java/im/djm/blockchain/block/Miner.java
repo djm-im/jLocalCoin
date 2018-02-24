99c9343d57220b269f7dc4b4666dbd0d0b2c627a
Djole <djordje.mijatovic.djole@gmail.com>
2018-02-24 01:29:48 +0100

    Important!! Bloch hash removed dependency from rawData -- header already contains DataHash

--- src/main/java/im/djm/blockchain/block/Miner.java
+++ src/main/java/im/djm/blockchain/block/Miner.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import im.djm.blockchain.BlockChain;
-import im.djm.blockchain.BlockUtil;
 import im.djm.blockchain.block.data.Data;
 import im.djm.blockchain.block.data.Validator;
 import im.djm.blockchain.hash.BlockHash;
@@ -16,55 +15,46 @@ import im.djm.blockchain.hash.DataHash;
  */
 public class Miner {
 
+	static Head createHead(Block prevBlock, Data data) {
+		BlockHash prevHash = prevBlock.getBlockHash();
+		long length = prevBlock.getLength() + 1;
+
+		byte[] rawData = data.getRawData();
+		DataHash dataHash = ByteArrayUtil.calculateDataHash(rawData);
+
+		return new Head(prevHash, length, dataHash);
+	}
+
 	public static Block createNewBlock(Block prevBlock, Data data) {
 		Head head = createHead(prevBlock, data);
 
-		BlockHash blockHash = findNonce(data, head);
+		BlockHash blockHash = findNonce(head);
 
-		// head - side effect
 		return Miner.createBlock(head, data, blockHash);
 	}
 
-	private static BlockHash findNonce(Data data, Head head) {
+	private static BlockHash findNonce(Head head) {
 		Validator<BlockHash> hashValidator = BlockChain.hashValidator;
 		List<Predicate<BlockHash>> hashValidationRules = BlockChain.hashValidationRules;
 
-		byte[] rawData = data.getRawData();
-		BlockHash blockHash = calcBlockHash(head, rawData);
+		BlockHash blockHash = calcBlockHash(head);
 		while (!hashValidator.isValid(blockHash, hashValidationRules)) {
-			blockHash = nextNonce(head, rawData);
+			head.incNonce();
+			blockHash = calcBlockHash(head);
 		}
 
 		return blockHash;
 	}
 
-	private static BlockHash nextNonce(Head head, byte[] rawData) {
-		head.incNonce();
+	private static BlockHash calcBlockHash(Head head) {
+		byte[] rawHead = head.getRawHead();
 
-		// TODO ???
-		// Should rawData of a data block be get each time
-		// a header contains hash of data
-		return calcBlockHash(head, rawData);
-	}
-
-	private static BlockHash calcBlockHash(Head head, byte[] rawData) {
-		byte[] rawBlock = BlockUtil.concatenateArrays(head.getRawHead(), rawData);
-
-		return ByteArrayUtil.calculateBlockHash(rawBlock);
+		return ByteArrayUtil.calculateBlockHash(rawHead);
 	}
 
 	// Factory method.
 	private static Block createBlock(Head head, Data data, BlockHash hash) {
 		return new Block(head, data, hash);
-	}
-
-	static Head createHead(Block prevBlock, Data data) {
-		BlockHash prevHash = prevBlock.getBlockHash();
-
-		long length = prevBlock.getLength() + 1;
-		DataHash dataHash = ByteArrayUtil.calculateDataHash(data.getRawData());
-
-		return new Head(prevHash, length, dataHash);
 	}
 
 }
