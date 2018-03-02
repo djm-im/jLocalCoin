e1eecaac360e8c3459f6c8920a4a51adbdc7672e
Djole <djordje.mijatovic.djole@gmail.com>
2018-03-03 00:58:38 +0100

    Added public at variables

--- src/main/java/im/djm/node/CmdConstants.java
+++ src/main/java/im/djm/node/CmdConstants.java
@@ -7,30 +7,30 @@ class CmdConstants {
 	// TODO??
 	// use enum
 
-	static final String CMD_HELP = "help";
+	public static final String CMD_HELP = "help";
 
-	static final String CMD_EXIT = "exit";
+	public static final String CMD_EXIT = "exit";
 
-	static final String CMD_WNEW = "wnew";
+	public static final String CMD_WNEW = "wnew";
 
-	static final String CMD_MWNEW = "mwnew";
+	public static final String CMD_MWNEW = "mwnew";
 
-	static final String CMD_WSTAT = "wstat";
+	public static final String CMD_WSTAT = "wstat";
 
-	static final String CMD_WDEL = "wdel";
+	public static final String CMD_WDEL = "wdel";
 
-	static final String CMD_WLIST = "wlist";
+	public static final String CMD_WLIST = "wlist";
 
-	static final String CMD_SEND = "send";
+	public static final String CMD_SEND = "send";
 
-	static final String CMD_MSEND = "msend";
+	public static final String CMD_MSEND = "msend";
 
-	static final String CMD_PRINT = "print";
+	public static final String CMD_PRINT = "print";
 
-	static final String CMD_PRINT_BLOCK = "block";
+	public static final String CMD_PRINT_BLOCK = "block";
 
-	static final String CMD_PRINT_UTXO = "utxo";
+	public static final String CMD_PRINT_UTXO = "utxo";
 
-	static final String CMD_PRINT_BC = "bc";
+	public static final String CMD_PRINT_BC = "bc";
 
 }
